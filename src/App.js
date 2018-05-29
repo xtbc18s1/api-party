0ca17fadb198399d6e8e72a2f93ee56e534d18a9
Dave Strus <dave@getfretless.com>
2018-05-29 10:19:45 -0400

    Add header content and style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>API Party</h1>
+        <div className="App-header">
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
+        </div>
       </div>
     );
   }
