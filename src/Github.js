1da82a86263a0b207f5f4dd1436607c81d3fa401
Dave Strus <dave@getfretless.com>
2018-05-29 15:18:38 -0400

    Tie value of input to state

--- src/Github.js
+++ src/Github.js
@@ -3,6 +3,14 @@ import React, { Component } from 'react'
 import './Github.css'
 
 class Github extends Component {
+  state = {
+    username: '',
+  }
+
+  handleChange = (ev) => {
+    this.setState({ username: ev.target.value })
+  }
+
   render() {
     return (
       <div className="Github">
@@ -13,7 +21,11 @@ class Github extends Component {
         />
         <form>
           <div>
-            <input type="text"/>
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <button type="submit">Look up GitHub user</button>
