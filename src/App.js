f1e26275638eb83961ce1e37844408bded4cdc1c
Dave Strus <dave@getfretless.com>
2018-05-29 16:25:13 -0400

    Add homework

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, NavLink } from 'react-router-dom'
 import './App.css'
 import Github from './Github'
 import Nasa from './Nasa'
+import Homework from './Homework'
 
 class App extends Component {
   render() {
@@ -20,10 +21,15 @@ class App extends Component {
           <li>
             <NavLink to="/nasa">NASA API</NavLink>
           </li>
+
+          <li>
+            <NavLink to="/homework">Homework</NavLink>
+          </li>
         </ul>
         <Switch>
           <Route path="/github" component={Github} />
           <Route path="/nasa" component={Nasa} />
+          <Route path="/homework" component={Homework} />
           <Route render={() => <p>To get started, click one of the links above.</p>} />
         </Switch>
       </div>
